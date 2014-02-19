19783813154bfbf8c7e8e7bcf672b88e0a0280f0
Philipp Seidel <phibo@dinotools.org>
2014-02-19 17:05:07 +0100

    src - Initial commit

---
+++ ArduRPC.cpp
@@ -0,0 +1,573 @@
+/**
+ * Arduino Remote Procedure Calls - ArduRPC
+ * Copyright (C) 2013-2014 DinoTools
+ *
+ * This file is part of ArduRPC.
+ *
+ * ArduRPC is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * ArduRPC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "ArduRPC.h"
+
+/**
+ * The constructor performs the following tasks.
+ *   - Allocate memory for the handler and function list
+ *   - Reset all counters
+ *   - Set rpc information
+ *
+ * The number of handlers and functions must not exceed the given number.
+ * It's not possible to increase the limits.
+ *
+ * @param handler_count Maximum number of handlers
+ * @param function_count Maximum number of functions
+ */
+ArduRPC::ArduRPC(uint8_t handler_count, uint8_t function_count)
+{
+  this->handlers = (rpc_handler_t *)malloc(sizeof(rpc_handler_t) * handler_count);
+  this->functions = (rpc_function_t *)malloc(sizeof(rpc_function_t) * function_count);
+  this->handler_infos = (rpc_handler_info_t *)malloc(sizeof(rpc_handler_info_t) * handler_count);
+  this->handler_index = 0;
+  this->function_index = 0;
+  this->max_handler_count = handler_count;
+  this->max_function_count = function_count;
+  this->data.data = (char *)malloc(RPC_MAX_DATA_LENGTH);
+#if RPC_SHARED_BUFFERS == 1
+  this->result.data = this->data.data;
+#else
+  this->result.data = (char *)malloc(RPC_MAX_RESULT_LENGTH);
+#endif
+}
+
+/**
+ * Connect a function to the RPC processor.
+ * @param function All information to call the function.
+ * @return The internal function index
+ */
+uint8_t ArduRPC::connectFunction(rpc_function_t function)
+{
+  if(this->function_index >= this->max_function_count - 1)
+    return 0xff;
+  this->functions[this->function_index] = function;
+  return this->function_index++;
+}
+
+/**
+ * Use the given parameters to create the required data structures and
+ * connect the function to the RPC processor.
+ * @param type The type of the function. Have a look at the type list in the documentation.
+ * @param callback The callback function to call
+ * @param arguments A pointer to the arguments. It is passed to the callback function
+ * @return The internal function index
+ */
+uint8_t ArduRPC::connectFunction(uint8_t type, void *callback, void *arguments)
+{
+  rpc_function_t function = {type, callback, arguments};
+  return connectFunction(function);
+}
+
+/**
+ * Connect a handler to the RPC processor.
+ * @param handler All information to call the handler.
+ * @return The internal handler index.
+ */
+uint8_t ArduRPC::connectHandler(rpc_handler_t handler)
+{
+  if(this->handler_index >= this->max_handler_count - 1)
+    return 0xff;
+  handlers[this->handler_index] = handler;
+  return this->handler_index++;
+}
+
+/**
+ * Use the given parameters to create the required data structures and
+ * connect the handler to the RPC processor.
+ * @param type The type of the handler. Have a look at the handler type list in the documentation.
+ * @param callback The callback function to call.
+ * @param arguments A pointer to the arguments. It is passed to the callback function.
+ * @return The internal handler index
+ */
+uint8_t ArduRPC::connectHandler(uint16_t type, void *callback, void *arguments)
+{
+  rpc_handler_t handler = {type, callback, arguments};
+  return connectHandler(handler);
+}
+
+/**
+ * Copy external data into the internal processing buffer
+ * @param src A pointer to the data to copy.
+ * @param len Number of bytes to copy
+ * @return 0 on success.
+ */
+uint8_t ArduRPC::copyData(uint8_t *src, uint8_t len)
+{
+  this->data.length = len;
+  memcpy(this->data.data, src, len);
+  return 0;
+}
+
+/**
+ * Read a character from the current position in the parameter data and return it.
+ * @return A character from the parameter data.
+ */
+char ArduRPC::getParam_char()
+{
+  char res = rpc_read_int8(&this->data.data[this->cur_data_read_pos]);
+  this->cur_data_read_pos++;
+  return res;
+}
+
+/**
+ * Read a byte from the current position in the parameter data and return it.
+ */
+int8_t ArduRPC::getParam_int8()
+{
+  int8_t res = rpc_read_int8(&this->data.data[this->cur_data_read_pos]);
+  this->cur_data_read_pos++;
+  return res;
+}
+
+/**
+ * Read two bytes from the current position in the parameter data and return it.
+ */
+int16_t ArduRPC::getParam_int16()
+{
+  int16_t res;
+  res = rpc_read_int16(&this->data.data[this->cur_data_read_pos]);
+  this->cur_data_read_pos += 2;
+  return res;
+}
+
+/**
+ * Read a string from the current position in the parameter data.
+ * @param dst Pointer to the destination.
+ * @param max_length The maximum number of bytes to copy.
+ * @return The number of copyed bytes.
+ */
+uint8_t ArduRPC::getParam_string(char *dst, uint8_t max_length)
+{
+  uint8_t length;
+  uint8_t n;
+  n = length = this->getParam_uint8();
+  if(n > max_length) {
+    n = max_length;
+  }
+  memcpy(dst, &this->data.data[this->cur_data_read_pos], n);
+  this->cur_data_read_pos += length;
+  return n;
+}
+
+/**
+ * Read a byte from the current position in the parameter data and return it.
+ */
+uint8_t ArduRPC::getParam_uint8()
+{
+  uint8_t res = rpc_read_uint8(&this->data.data[this->cur_data_read_pos]);
+  this->cur_data_read_pos++;
+  return res;
+}
+
+/**
+ * Read two bytes from the current position in the parameter data and return it.
+ */
+uint16_t ArduRPC::getParam_uint16()
+{
+  uint16_t res;
+  res = rpc_read_uint16(&this->data.data[this->cur_data_read_pos]);
+  this->cur_data_read_pos += 2;
+  return res;
+}
+
+/**
+ * Return a pointer to the result data
+ * @return A pointer to the result data
+ */
+uint8_t *ArduRPC::getResultData()
+{
+  return (uint8_t *)this->result.data;
+}
+
+/**
+ * Get the length of the result data including the header.
+ * @return The number of bytes of the result data
+ */
+uint8_t ArduRPC::getResultLength()
+{
+  return this->result.length + 1;
+}
+
+/**
+ * Get the length of the result data.
+ * @return The number of bytes of the result data
+ */
+uint8_t ArduRPC::getResultDataLength()
+{
+  return this->result.length;
+}
+
+/**
+ * Handle all system calls.
+ * @param cmd_id The ID of the called command.
+ * @return The result of the command.
+ */
+uint8_t ArduRPC::handleSystemCalls(uint8_t cmd_id)
+{
+  uint8_t i;
+  uint16_t handler_type;
+  uint8_t handler_id;
+
+  if (cmd_id == 0x01) {
+    // get protocol version
+    this->writeResult(RPC_UINT8);
+    this->writeResult(0);
+    return RPC_RETURN_SUCCESS;
+  } else if (cmd_id == 0x02) {
+    // get library version
+    this->writeResult(RPC_ARRAY);
+    this->writeResult(RPC_UINT8);
+    this->writeResult(3);
+    this->writeResult(RPC_VERSION_MAJOR);
+    this->writeResult(RPC_VERSION_MINOR);
+    this->writeResult(RPC_VERSION_PATCH);
+    return RPC_RETURN_SUCCESS;
+  } else if (cmd_id == 0x03) {
+    this->writeResult_uint16(RPC_MAX_DATA_LENGTH);
+    return RPC_RETURN_SUCCESS;
+  } else if (cmd_id == 0x10) {
+    this->writeResult(RPC_MCARRAY);
+    this->writeResult(2);
+    this->writeResult(RPC_UINT8);
+    this->writeResult(RPC_UINT8);
+    this->writeResult(function_index);
+    for (i = 0; i < function_index; i++) {
+      this->writeResult(i);
+      this->writeResult(this->functions[i].type);
+    }
+    return RPC_RETURN_SUCCESS;
+  } else if (cmd_id == 0x20) {
+    this->writeResult(RPC_MCARRAY);
+    this->writeResult(2);
+    this->writeResult(RPC_UINT8);
+    this->writeResult(RPC_UINT16);
+    this->writeResult(handler_index);
+    for (i = 0; i < handler_index; i++) {
+      handler_type = this->handlers[i].type;
+      this->writeResult(i);
+      this->writeResult((((handler_type) >> 8) & 0xff));
+      this->writeResult(((handler_type) & 0xff));
+    }
+    return RPC_RETURN_SUCCESS;
+  } else if (cmd_id == 0x21) {
+    handler_id = this->getParam_int8();
+    if (handler_id < this->max_handler_count) {
+      this->writeResult(RPC_STRING);
+      this->writeResult(RPC_MAX_NAME_LENGTH);
+      this->writeResult(this->handler_infos[handler_id].name, RPC_MAX_NAME_LENGTH);
+    }
+    return RPC_RETURN_SUCCESS;
+  }
+  return RPC_RETURN_COMMAND_NOT_FOUND;
+}
+
+/**
+ * Process the data in the internal processing buffer.
+ *   -# Check if the protocol version is supported.
+ *   -# Extract the handler ID.
+ *   -# Extract the command ID.
+ *   -# Extract the length of the parameter data.
+ *   -# Call the requested handler and function.
+ */
+void ArduRPC::process()
+{
+  // reset result
+  this->result.length = 0;
+
+  // protocoll version
+  if (this->getParam_uint8() != 0x00) {
+    return;
+  }
+
+  uint8_t handler_id = this->getParam_uint8();
+  uint8_t command_id = this->getParam_uint8();
+  uint8_t length = this->getParam_uint8();
+  uint8_t res = RPC_RETURN_FAILURE;
+
+  if(handler_id < this->handler_index) {
+    rpc_handler_t *handler;
+    handler = &handlers[handler_id];
+
+    //res = handler->callback(command_id, this, handler->arguments);
+    rpc_callback_handler_t callback = (rpc_callback_handler_t)handler->callback;
+    res = callback(command_id, this, handler->arguments);
+  } else if (handler_id == 0xfe) {
+    if (command_id >= this->function_index) {
+      res = RPC_RETURN_FUNCTION_NOT_FOUND;
+    } else {
+      rpc_function_t *function;
+      function = &functions[handler_id];
+      rpc_callback_function_t callback_function = (rpc_callback_function_t)function->callback;
+      res = callback_function(this, function->arguments);
+    }
+  } else if (handler_id == 0xff) {
+    res = this->handleSystemCalls(command_id);
+  } else {
+    res = RPC_RETURN_HANDLER_NOT_FOUND;
+  }
+
+  this->setReturnCode(res);
+  if (this->getResultDataLength() == 0) {
+    this->writeResult(RPC_NONE);
+  }
+}
+
+/**
+ * Read and return the next byte from the result buffer.
+ * @return The next byte from result buffer.
+ */
+uint8_t ArduRPC::readResult()
+{
+  return this->result.data[this->cur_result_read_pos++];
+}
+
+/**
+ * Reset the internal processing and result buffer.
+ * It does *not* remove connected handlers or functions.
+ */
+void ArduRPC::reset() {
+  this->result.length = 0;
+  this->data.length = 0;
+  this->cur_data_read_pos = 0;
+  this->cur_result_read_pos = 0;
+}
+
+/**
+ * Set the name of a handler.
+ * The length of the name must not exeed the number of bytes specified in RPC_MAX_NAME_LENGTH.
+ * A name must only be used once.
+ * @see RPC_MAX_NAME_LENGTH
+ * @see connectHandler()
+ * @param handler_id The internal handler ID returned while the handler was connected.
+ * @param name The name of the handler.
+ */
+bool ArduRPC::setHandlerName(uint8_t handler_id, char name[])
+{
+  if(handler_id < this->max_handler_count) { 
+    strncpy(this->handler_infos[handler_id].name, name, RPC_MAX_NAME_LENGTH);
+  }
+}
+
+/**
+ * Set the return code.
+ * @param code The return code
+ */
+void ArduRPC::setReturnCode(uint8_t code)
+{
+  this->result.data[0] = code;
+}
+
+/**
+ * Write a byte into the data buffer.
+ * @param c The byte to write.
+ */
+bool ArduRPC::writeData(uint8_t c)
+{
+  this->data.data[this->data.length] = c;
+  this->data.length++;
+}
+
+/**
+ * Write a byte into the result buffer.
+ * @param c The byte to write.
+ */
+bool ArduRPC::writeResult(uint8_t c)
+{
+  this->result.length++;
+  this->result.data[this->result.length] = c;
+}
+
+/**
+ * Write a string into the result buffer.
+ * @param string A pointer to the string.
+ * @param length The length of the string to copy.
+ */
+bool ArduRPC::writeResult(char *string, uint8_t length)
+{
+  memcpy(&this->result.data[this->result.length + 1], string, RPC_MAX_NAME_LENGTH);
+  this->result.length = this->result.length + length;
+}
+
+/**
+ * Write a value of type INT8
+ * @param value The value to write.
+ */
+bool ArduRPC::writeResult_int8(int8_t value)
+{
+  this->writeResult(RPC_INT8);
+  this->writeResult(value);
+}
+
+/**
+ * Write a value of type INT16
+ * @param value The value to write.
+ */
+bool ArduRPC::writeResult_int16(int16_t value)
+{
+  this->writeResult(RPC_INT16);
+  this->writeResult((((value) >> 8) & 0xff));
+  this->writeResult(((value) & 0xff));
+}
+
+/**
+ * Write a value of type STRING
+ * @param value The value to write.
+ * @param length The string length.
+ */
+bool ArduRPC::writeResult_string(char *value, uint8_t length)
+{
+  this->writeResult(RPC_STRING);
+  this->writeResult(length);
+  this->writeResult(value, length);
+}
+
+/**
+ * Write a value of type UINT8
+ * @param value The value to write.
+ */
+bool ArduRPC::writeResult_uint8(uint8_t value)
+{
+  this->writeResult(RPC_UINT8);
+  this->writeResult(value);
+}
+
+/**
+ * Write a value of type UINT16
+ * @param value The value to write.
+ */
+bool ArduRPC::writeResult_uint16(uint16_t value)
+{
+  this->writeResult(RPC_UINT16);
+  this->writeResult((((value) >> 8) & 0xff));
+  this->writeResult(((value) & 0xff));
+}
+
+/**
+ * The constructor.
+ *
+ * @param serial: Specify the serial port to use
+ * @param rpc: Specify the rpc handler to use
+ *
+ */
+ArduRPCSerial::ArduRPCSerial(Stream &serial, ArduRPC &rpc)
+{
+  this->_serial = &serial;
+  this->_rpc = &rpc;
+  this->_state = 0;
+}
+
+/**
+ * Replace the main loop.
+ *
+ * Can be used if the main loop() only calls readData().
+ *
+ * This function will never return.
+ */
+void ArduRPCSerial::loop()
+{
+  while(1)
+    readData();
+}
+
+/**
+ * Process serial data encoded in hex format.
+ *
+ * @param c: Character to process
+ */
+void ArduRPCSerial::processDataHex(uint8_t c)
+{
+  if(c == '\r')
+    return;
+
+  if(c == '\n') {
+    this->_rpc->process();
+    this->processResultHex();
+    this->_state = 0;
+    return;
+  }
+
+  if(c >= 97) {
+    c -= 87;
+  } 
+  else if(c >= 65) {
+    c -= 55;
+  } 
+  else {
+    c -= 48;
+  }
+
+  if(this->_tmp_data_part == 0) {
+    c = c << 4;
+    this->_tmp_data = c;
+    this->_tmp_data_part = 1;
+  } else {
+    this->_tmp_data |= c;
+    this->_rpc->writeData(this->_tmp_data);
+    this->_tmp_data_part = 0;
+  }
+}
+
+/**
+ * Process the result and write it to the serial port using the HEX encoding.
+ */
+void ArduRPCSerial::processResultHex()
+{
+    uint8_t i;
+    uint8_t len;
+    uint8_t *data;
+
+    len = this->_rpc->getResultLength();
+    if (len == 0)
+      return;
+
+    this->_serial->println(len);
+    this->_serial->print(":");
+    data = this->_rpc->getResultData();
+    for (i = 0; i < len; i++) {
+      if (data[0] < 0x10)
+        this->_serial->print('0');
+      this->_serial->print(data[0], HEX);
+      data++;
+    }
+    this->_serial->print('\n');
+}
+
+/**
+ * Read and process data from the serial port specified
+ */
+void ArduRPCSerial::readData()
+{
+  uint8_t c;
+
+  if (this->_serial->available() < 1)
+    return;
+
+  c = this->_serial->read();
+
+  if (this->_state == 1)
+    processDataHex(c);
+
+  if (this->_state == 0 && c == ':') {
+    this->_state = 1;
+    this->_rpc->reset();
+    this->_tmp_data_part = 0;
+  }
+}
---
+++ ArduRPC.h
@@ -0,0 +1,334 @@
+/**
+ * Arduino Remote Procedure Calls - ArduRPC
+ * Copyright (C) 2013-2014 DinoTools
+ *
+ * This file is part of ArduRPC.
+ *
+ * ArduRPC is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * ArduRPC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef ARDURPC_H
+#define ARDURPC_H
+
+#if (ARDUINO >= 100)
+ #include <Arduino.h>
+#else
+ #include <WProgram.h>
+ #include <pins_arduino.h>
+#endif
+
+/* Config Start */
+
+//! Set to 1 to share memory between data and result buffer
+#define RPC_SHARED_BUFFERS 1
+
+//! Maximum number of bytes available for data or shared buffer
+#define RPC_MAX_DATA_LENGTH 128
+
+//! Maximum number of bytes available for result buffer.
+/*! Only change if RPC_SHARED_BUFFERS is set to 0 */
+#define RPC_MAX_RESULT_LENGTH RPC_MAX_DATA_LENGTH
+
+//! Maximum number characters available for each handler name
+/*! Keep it as small as possible and don't waste memory */
+#define RPC_MAX_NAME_LENGTH 16
+
+/* Config end */
+
+//! Major version
+#define RPC_VERSION_MAJOR 0
+
+//! Minor version
+#define RPC_VERSION_MINOR 3
+
+//! Patch level
+#define RPC_VERSION_PATCH 0
+
+//! Version
+#define RPC_VERSION (RPC_VERSION_MAJOR * 10000 \
+		   + RPC_VERSION_MINOR * 100 \
+		   + RPC_VERSION_PATCH)
+
+
+//! Datatype identifier
+#define RPC_NONE 0x00
+//! Datatype identifier
+#define RPC_INT8 0x01
+//! Datatype identifier
+#define RPC_UINT8 0x02 // char
+//! Datatype identifier
+#define RPC_INT16 0x03
+//! Datatype identifier
+#define RPC_UINT16 0x04
+//! Datatype identifier
+#define RPC_INT32 0x05
+//! Datatype identifier
+#define RPC_UINT32 0x06
+//! Datatype identifier
+#define RPC_INT64 0x07
+//! Datatype identifier
+#define RPC_UINT64 0x08
+//! Datatype identifier
+#define RPC_ARRAY 0x10
+//! Datatype identifier
+#define RPC_STRING 0x11
+//! Datatype identifier
+#define RPC_MCARRAY 0x12  // 
+//! Datatype identifier
+#define RPC_VARRAY 0x13  // 
+
+//! The command has been executed successfully
+#define RPC_RETURN_SUCCESS 0
+//! Unable determine the requested function
+#define RPC_RETURN_FUNCTION_NOT_FOUND 124
+//! Unable determine the requested handler
+#define RPC_RETURN_HANDLER_NOT_FOUND 125
+//! Unable determine the requested command
+#define RPC_RETURN_COMMAND_NOT_FOUND 126
+//! An error occurred 
+#define RPC_RETURN_FAILURE 127
+
+//! Type is used for the processing buffer
+typedef struct {
+  //! The length of the data
+  uint8_t length;
+  //! The data of the buffer
+  char *data;
+} rpc_data_t;
+
+//! Type is used for the result buffer
+typedef struct {
+  //! The length of the data
+  uint8_t length;
+  //! The data of the buffer
+  char *data;
+} rpc_result_t;
+
+//! Used to store main information about a rpc function
+typedef struct {
+  //! The type of the rpc function. See documentation for more information.
+  uint8_t type;
+  //! A pointer to the callback function.
+  void *callback;
+  //! A pointer to the arguments passed to the callback.
+  void *arguments;
+} rpc_function_t;
+
+//! Used to store main information about a rpc handler.
+typedef struct {
+  //! The type of the rpc handler. See documentation for more information.
+  uint16_t type;
+  //! A pointer to the callback function of the handler.
+  void *callback;
+  //! A pointer to the arguments passed to the callback.
+  void *arguments;
+} rpc_handler_t;
+
+//! Additional information about a rpc handler. But not used for rpc functions.
+typedef struct {
+  //! The name of the handler
+  char name[RPC_MAX_NAME_LENGTH];
+  //! Internal address where data section starts in config
+  uint16_t config_address;
+  //! Internal address where data section starts in eeprom
+  uint16_t eeprom_address;
+} rpc_handler_info_t;
+
+/**
+ * The main class to handle rpc on a microcontroller.
+ *
+ * It's possible to:
+ *   - register handlers and functions
+ *   - process incoming data
+ *     - identify called handler and function
+ *     - extract parameters
+ *   - generate and return the result
+ *   - query the system and get additional information
+ *     - available handlers and functions
+ *     - name of a handler
+ */
+
+class ArduRPC
+{
+  public:
+    ArduRPC(uint8_t handler_count=8, uint8_t function_count=8);
+    bool
+      setHandlerName(uint8_t handler_id, char name[]),
+      writeData(uint8_t c),
+      writeResult(uint8_t c),
+      writeResult(char *string, uint8_t length),
+      writeResult_int8(int8_t value),
+      writeResult_int16(int16_t value),
+      writeResult_string(char *string, uint8_t length),
+      writeResult_uint8(uint8_t value),
+      writeResult_uint16(uint16_t value);
+    uint8_t
+      connectFunction(rpc_function_t function),
+      connectFunction(uint8_t type, void *callback, void *arguments),
+      connectHandler(rpc_handler_t handler),
+      connectHandler(uint16_t type, void *callback, void *arguments),
+      readResult(),
+      *getResultData(),
+      copyData(uint8_t *src, uint8_t len),
+      getResultLength(),
+      getResultDataLength();
+    void
+      process(),
+      reset(),
+      setReturnCode(uint8_t code);
+    // get params
+    char
+      getParam_char();
+    int8_t
+      getParam_int8();
+    int16_t
+      getParam_int16();
+    uint8_t
+      getParam_uint8(),
+      getParam_string(char *dst, uint8_t max_length);
+    uint16_t
+      getParam_uint16();
+  private:
+    /* functions */
+    uint8_t
+      handleSystemCalls(uint8_t cmd_id);
+
+    /* vars */
+    rpc_handler_t
+      //! List of connected rpc handlers
+      *handlers;
+    rpc_function_t
+      //! List of connected rpc functions
+      *functions;
+
+    rpc_result_t
+      //! Result buffer
+      result;
+    rpc_data_t
+      //! Data buffer
+      data;
+
+    rpc_handler_info_t
+      //! Additional information for connected handlers
+      *handler_infos;
+
+    // internal stuff
+    uint8_t
+      //! Current position in the data buffer while reading data
+      cur_data_read_pos,
+      //! Current position in the result buffer while reading data
+      cur_result_read_pos,
+      //! Number of connected handlers
+      handler_index,
+      //! Number of connected functions
+      function_index,
+      //! Maximum number of connected handlers
+      max_handler_count,
+      //! Maximum number of connected functions
+      max_function_count;
+};
+
+/**
+ * Handle serial communication.
+ */
+class ArduRPCSerial
+{
+  public:
+    ArduRPCSerial(Stream &serial, ArduRPC &rpc);
+    void loop();
+    void processDataHex(uint8_t c);
+    void readData();
+  private:
+    //! RPC handler to use
+    ArduRPC *_rpc;
+    //! Serial port to use
+    Stream *_serial;
+    //! Internal processing state
+    uint8_t _state;
+    //! Temporary data
+    uint8_t _tmp_data;
+    //! Data part for hex strings. 0 = part 1 (bits 7-4); 1 = part 2 (bits 3-0)
+    uint8_t _tmp_data_part;
+    void processResultHex();
+};
+
+//! Callback function for a rpc function
+typedef uint8_t (*rpc_callback_function_t)(ArduRPC *rpc, void *);
+//! Callback function for a rpc handler
+typedef uint8_t (*rpc_callback_handler_t)(uint8_t, ArduRPC *rpc, void *);
+
+/**
+ * Extract one byte from given data
+ * @param data array
+ * @return Extracted data
+ */
+static inline int8_t rpc_read_char(void *data)
+{
+  char *d = (char *)data;
+  return (char)d[0];
+}
+
+/**
+ * Extract one byte from given data
+ * @param data array
+ * @return Extracted data
+ */
+static inline int8_t rpc_read_int8(void *data)
+{
+  int8_t *d = (int8_t *)data;
+  return (int8_t)d[0];
+}
+
+/**
+ * Extract two bytes from given data
+ * @param data array
+ * @return Extracted data
+ */
+static inline int16_t rpc_read_int16(void *data)
+{
+  int16_t res;
+  uint8_t *d = (uint8_t *)data;
+  res = d[0];
+  res = res << 8;
+  res = res | d[1];
+  return res;
+}
+
+/**
+ * Extract one byte from given data
+ * @param data array
+ * @return Extracted data
+ */
+static inline uint8_t rpc_read_uint8(void *data)
+{
+  uint8_t *d = (uint8_t *)data;
+  return (uint8_t)d[0];
+}
+
+/**
+ * Extract two byte from given data
+ * @param data array
+ * @return Extracted data
+ */
+static inline uint16_t rpc_read_uint16(void *data)
+{
+  uint16_t res;
+  uint8_t *d = (uint8_t *)data;
+  res = d[0];
+  res = res << 8;
+  res = res | d[1];
+  return res;
+}
+
+#endif
